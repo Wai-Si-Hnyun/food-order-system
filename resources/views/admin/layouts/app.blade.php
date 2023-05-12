92f5ef4c4e443c56e632faedf4ef29aece809034
Piccolo27 <waisihnyun27@gmail.com>
2023-05-12 14:15:18 +0630

    Restore auth layers and authorization middleware

--- resources/views/admin/layouts/app.blade.php
+++ resources/views/admin/layouts/app.blade.php
@@ -3,55 +3,7 @@
 <html lang="en" class="light-style layout-menu-fixed" dir="ltr" data-theme="theme-default"
     data-assets-path="../assets/" data-template="vertical-menu-template-free">
 
-<head>
-    <meta charset="utf-8" />
-    <meta name="viewport"
-        content="width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0" />
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>Cake Shop | Admin</title>
-
-    <meta name="description" content="" />
-
-    <!-- Favicon -->
-    <link rel="icon" type="image/x-icon" href="{{ asset('assets/admin/img/favicon/favicon.ico') }}" />
-
-    <!-- Fonts -->
-    <link rel="preconnect" href="https://fonts.googleapis.com" />
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-    <link
-        href="https://fonts.googleapis.com/css2?family=Public+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap"
-        rel="stylesheet" />
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"
-        integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw=="
-        crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <!-- Icons. Uncomment required icon fonts -->
-    <link rel="stylesheet" href="{{ asset('assets/admin/vendor/fonts/boxicons.css') }}" />
-
-    <!-- Core CSS -->
-    <link rel="stylesheet" href="{{ asset('assets/admin/vendor/css/core.css') }}"
-        class="template-customizer-core-css" />
-    <link rel="stylesheet" href="{{ asset('assets/admin/vendor/css/theme-default.css') }}"
-        class="template-customizer-theme-css" />
-    <link rel="stylesheet" href="{{ asset('assets/admin/css/demo.css') }}" />
-
-    {{-- font awesome --}}
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"
-        integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw=="
-        crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <!-- Vendors CSS -->
-    <link rel="stylesheet" href="{{ asset('assets/admin/vendor/libs/perfect-scrollbar/perfect-scrollbar.css') }}" />
-
-    <link rel="stylesheet" href="{{ asset('assets/admin/vendor/libs/apex-charts/apex-charts.css') }}" />
-
-    <!-- Page CSS -->
-
-    <!-- Helpers -->
-    <script src="{{ asset('assets/admin/vendor/js/helpers.js') }}"></script>
-
-    <!--! Template customizer & Theme config files MUST be included after core stylesheets and helpers.js') }} in the <head> section -->
-    <!--? Config:  Mandatory theme config file contain global vars & default theme options, Set your preferred theme option in this file.  -->
-    <script src="{{ asset('assets/admin/js/config.js') }}"></script>
-</head>
+@include('admin.common.head')
 
 <body>
     <!-- Layout wrapper -->
@@ -76,31 +28,7 @@
             </div>
         </div>
 
-        <!-- Core JS -->
-        <!-- build:js assets/vendor/js/core.js') }} -->
-        <script src="{{ asset('assets/admin/vendor/libs/jquery/jquery.js') }}"></script>
-        <script src="{{ asset('assets/admin/vendor/libs/popper/popper.js') }}"></script>
-        <script src="{{ asset('assets/admin/vendor/js/bootstrap.js') }}"></script>
-        <script src="{{ asset('assets/admin/vendor/libs/perfect-scrollbar/perfect-scrollbar.js') }}"></script>
-
-        <script src="{{ asset('assets/admin/vendor/js/menu.js') }}"></script>
-        <!-- endbuild -->
-
-        <!-- Vendors JS -->
-        <script src="{{ asset('assets/admin/vendor/libs/apex-charts/apexcharts.js') }}"></script>
-
-        <!-- Main JS -->
-        <script src="{{ asset('assets/admin/js/main.js') }}"></script>
-
-        <!-- Page JS -->
-        <script src="{{ asset('assets/admin/js/dashboards-analytics.js') }}"></script>
-
-        <!-- Place this tag in your head or just before your close body tag. -->
-        <script async defer src="https://buttons.github.io/buttons.js') }}"></script>
-
-        <script src="{{ asset('js/app.js') }}"></script>
-
-        <script src="{{ asset('js/admin/menu.js') }}"></script>
+        @include('admin.common.js')
 
         @stack('script')
 </body>
--- routes/web.php
+++ routes/web.php
@@ -1,56 +1,55 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\HomeController;
 use App\Http\Controllers\OrderController;
 use App\Http\Controllers\PaymentController;
 use App\Http\Controllers\ProductController;
-use App\Http\Controllers\CategoryController;
+use Illuminate\Support\Facades\Route;
 
-Route::middleware(['auth:sanctum',config('jetstream.auth_session'),'verified'])->group(function () {
-    Route::get('/dashboard', function () {return view('dashboard');})->name('dashboard');
-});
+// Route::middleware(['auth:sanctum', config('jetstream.auth_session'), 'verified'])->group(function () {
+//     Route::get('/dashboard', function () {return view('dashboard');})->name('dashboard');
+// });
+
+Route::get('/', [HomeController::class, 'home'])->name('home');
 
 //login/register
-Route::get('/login',[AuthController::class,'login'])->name('auth#login');
-Route::get('/registerPage',[AuthController::class,'registerPage'])->name('auth#registerPage');
+Route::get('/login', [AuthController::class, 'login'])->name('auth#login');
+Route::get('/registerPage', [AuthController::class, 'registerPage'])->name('auth#registerPage');
+Route::post('/Registerstore', [AuthController::class, 'authRegisterStore'])->name('auth#store');
+Route::post('/loginStore', [AuthController::class, 'authLogin'])->name('auth#loginCheck');
 
-Route::post('/Registerstore',[AuthController::class,'authRegisterStore'])->name('auth#store');
-Route::post('/loginStore',[AuthController::class,'authLogin'])->name('auth#loginCheck');
-
-Route::prefix('category')->middleware('auth')->group(function(){
-    Route::get('list',[CategoryController::class,'list'])->name('category#list');
+Route::middleware('role:user')->group(function () {
+    // Order
+    Route::post('user/order/create', [OrderController::class, 'store'])->name('order#store');
+    Route::get('/checkout', [OrderController::class, 'checkout'])->name('checkout');
+    Route::get('/payment/status', [PaymentController::class, 'status']);
 });
 
-// for category
-Route::get('/admin/categories', [CategoryController::class, 'index'])->name('categories#index');
-Route::get('/create', [CategoryController::class, 'create'])->name('categories.create');
-Route::post('/store', [CategoryController::class, 'store'])->name('categories.store');
-Route::get('/categories/edit/{id}', [CategoryController::class, 'edit'])->name('categories.edit');
-Route::post('/categories/update/{id}', [CategoryController::class, 'update'])->name('categories.update');
-Route::get('/categories/delete/{id}', [CategoryController::class, 'destroy'])->name('categories.destroy');
+Route::middleware('role:admin')->group(function () {
+    // Dashboard
+    Route::get('/admin/dashboard', [HomeController::class, 'adminDashboard'])->name('admin#dashboard');
+    
+    // for category
+    Route::get('/admin/categories', [CategoryController::class, 'index'])->name('categories#index');
+    Route::get('/create', [CategoryController::class, 'create'])->name('categories.create');
+    Route::post('/store', [CategoryController::class, 'store'])->name('categories.store');
+    Route::get('/categories/edit/{id}', [CategoryController::class, 'edit'])->name('categories.edit');
+    Route::post('/categories/update/{id}', [CategoryController::class, 'update'])->name('categories.update');
+    Route::get('/categories/delete/{id}', [CategoryController::class, 'destroy'])->name('categories.destroy');
 
-// for product
-Route::get('/admin/products', [ProductController::class, 'index'])->name('products#index');
-Route::get('/products/create', [ProductController::class, 'create'])->name('products.create');
-Route::post('/products/store', [ProductController::class, 'store'])->name('products.store');
-Route::get('/product/details/{id}', [ProductController::class, 'detail'])->name('products.details');
-Route::get('/products/edit/{id}', [ProductController::class, 'edit'])->name('products.edit');
-Route::post('/products/update/{id}', [ProductController::class, 'update'])->name('products.update');
-Route::get('/products/delete/{id}', [ProductController::class, 'destroy'])->name('products.destroy');
+    // for product
+    Route::get('/admin/products', [ProductController::class, 'index'])->name('products#index');
+    Route::get('/products/create', [ProductController::class, 'create'])->name('products.create');
+    Route::post('/products/store', [ProductController::class, 'store'])->name('products.store');
+    Route::get('/product/details/{id}', [ProductController::class, 'detail'])->name('products.details');
+    Route::get('/products/edit/{id}', [ProductController::class, 'edit'])->name('products.edit');
+    Route::post('/products/update/{id}', [ProductController::class, 'update'])->name('products.update');
+    Route::get('/products/delete/{id}', [ProductController::class, 'destroy'])->name('products.destroy');
 
-// Order
-Route::get('/admin/orders', [OrderController::class, 'index'])->name('orders#index');
-Route::get('/admin/order/{id}/show', [OrderController::class, 'show'])->name('orders#show');
-Route::post('user/order/create', [OrderController::class, 'store'])->name('order#store');
-Route::delete('admin/order/{id}/delete', [OrderController::class, 'destroy'])->name('order#delete');
-Route::get('/checkout', [OrderController::class, 'checkout'])->name('checkout');
-Route::get('/payment/status', [PaymentController::class, 'status']);
-
-Route::middleware(['role:user'])->group(function () {
-    //Define user routes here
-});
-
-Route::middleware(['role:admin'])->group(function () {
-    //Define admin routes here
+    // Order
+    Route::get('/admin/orders', [OrderController::class, 'index'])->name('orders#index');
+    Route::get('/admin/order/{id}/show', [OrderController::class, 'show'])->name('order#show');
+    Route::delete('admin/order/{id}/delete', [OrderController::class, 'destroy'])->name('order#delete');
 });
