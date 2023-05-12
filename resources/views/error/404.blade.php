4cbecbfb78d6c5cdad3b9ef91f60f76e7caf4933
Piccolo27 <waisihnyun27@gmail.com>
2023-05-12 14:29:31 +0630

    Edit 404 page

--- resources/views/error/404.blade.php
+++ resources/views/error/404.blade.php
@@ -34,15 +34,6 @@
 
     <!-- / Content -->
 
-    <div class="buy-now">
-      <a
-        href="https://themeselection.com/products/sneat-bootstrap-html-admin-template/"
-        target="_blank"
-        class="btn btn-danger btn-buy-now"
-        >Upgrade to Pro</a
-      >
-    </div>
-
     @include('admin.common.js')
   </body>
 </html>
