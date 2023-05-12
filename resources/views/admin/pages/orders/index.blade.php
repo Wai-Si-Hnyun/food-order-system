6d967a6ca1ef69d2a47b3b0955bb2485e7eee92d
Piccolo27 <waisihnyun27@gmail.com>
2023-05-13 01:09:32 +0630

    Update layouts with header and footer

--- resources/views/admin/pages/orders/index.blade.php
+++ resources/views/admin/pages/orders/index.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
     <div class="container-xxl flex-grow-1 container-p-y">
-        <h4 class="fw-bold py-3 mb-4"><span class="text-muted fw-light">Orders /</span> Order List</h4>
+        <h4 class="fw-bold py-3 mb-4">Orders</h4>
         <div class="card">
             <div class="table-responsive text-nowrap">
                 <table class="table">
