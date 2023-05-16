ba7dc2ecfd68eb6a025ac6b57c336aacfa2d3a81
Myat-Pyae-Sone <myat2000k77@gmail.com>
2023-05-16 15:32:53 +0630

    error reporting

--- resources/views/admin/pages/product/details.blade.php
+++ resources/views/admin/pages/product/details.blade.php
@@ -10,14 +10,14 @@
             </div>
             <div class="row card-body">
                 <div class="col-3">
-                    <img class="w-80" src="{{ asset('storage/' . $product->image) }}" class="img-thumbnail shadow-sm">
+                    <img src="{{ asset('storage/' . $product->image) }}" class="img-thumbnail shadow-sm ms-5">
                 </div>
                 <div class="col-5 offset-2">
                     <h5 class="mb-4"><i class="fa-solid fa-clipboard-list me-3"></i>{{ $product->category_id }}</h5>
                     <h5 class="mb-4"><i class="fa-solid fa-utensils me-3"></i>{{ $product->name }}</h5>
                     <h5 class="mb-4"><i class="fa-solid fa-money-bill-wave me-3"></i>${{ $product->price }}</h5>
                     <h5 class="mb-4"><i class="fa-regular fa-calendar-days me-3">
-                        </i>{{ $product->created_at->format('J-F-Y') }}
+                        </i>{{ $product->created_at->format('j-F-Y') }}
                     </h5>
                     <h5 class="mb-4"><i class="fa-solid fa-receipt me-3"></i>{{ $product->description }}</h5>
                 </div>
