49412c381b8f86333f6f270fb708e157ea33ab62
Myat-Pyae-Sone <myat2000k77@gmail.com>
2023-05-19 10:29:07 +0630

    admin logout add a tag

--- resources/views/admin/layouts/app.blade.php
+++ resources/views/admin/layouts/app.blade.php
@@ -226,6 +226,7 @@
                                             <i class="bx bx-power-off me-2"></i>
                                             <span class="align-middle">Log Out</span>
                                         </a>
+                                        </a>
                                     </li>
                                 </ul>
                             </li>
