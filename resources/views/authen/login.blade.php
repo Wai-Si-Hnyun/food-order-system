e0f467de4c74c16e4a2b41e120c79327498b70be
Myat-Pyae-Sone <myat2000k77@gmail.com>
2023-05-23 09:43:54 +0630

    changes commit

--- resources/views/authen/login.blade.php
+++ resources/views/authen/login.blade.php
@@ -2,12 +2,12 @@
 
 @section('content')
     @if (session('alert'))
-    <div class="alert alert-warning alert-dismissible fade show" role="alert">
-       <p>Email or Password may be wrong.Please try again!</p>
-        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-            <span aria-hidden="true">&times;</span>
-        </button>
-    </div>
+        <div class="alert alert-warning alert-dismissible fade show" role="alert">
+            <p>Email or Password may be wrong.Please try again!</p>
+            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+            </button>
+        </div>
     @endif
 
     <div class="login-form">
@@ -15,7 +15,8 @@
             @csrf
             <div class="form-group">
                 <label>Email Address <span class="text-danger">*</span></label>
-                <input class="form-control" type="email" name="email" placeholder="example@gmail.com" value="{{old('email')}}">
+                <input class="form-control" type="email" name="email" placeholder="example@gmail.com"
+                    value="{{ old('email') }}">
             </div>
             @error('email')
                 <span class="text-danger">{{ $message }}</span>
@@ -33,7 +34,7 @@
             @if ($errors->has('g-recaptcha-response'))
                 <span class="text-danger">{{ $errors->first('g-recaptcha-response') }}</span>
             @endif
-            <a href="{{route('auth.forgetPass')}}" class="mb-3 text-primary d-block">forget password?</a>
+            <a href="{{ route('auth.forgetPass') }}" class="mb-3 text-primary d-block">forget password?</a>
             <button class="text-light pt-2 pb-2 au-btn--block au-btn--orange m-b-20" type="submit">Sign In</button>
         </form>
         <div class="register-link">
